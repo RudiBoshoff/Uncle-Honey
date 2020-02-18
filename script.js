c5ca0fa871c1b54cd98a8a2cb0de8073b0f0c012
Rudi Boshoff <rudiboshoff15@gmail.com>
2020-02-18 16:40:48 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// TOGGLE MENU
+$(function() {
+  $(".menu__toggle").click(function() {
+    $(".menu__items").toggleClass("menu__items--display");
+    $(this)
+      .find("a")
+      .html("<i class='fas fa-bars'></i>");
+
+    if ($(".menu__items").hasClass("menu__items--display")) {
+      $(this)
+        .find("a")
+        .html("<i class='fas fa-times'></i>");
+    }
+  });
+});
+
+// SWIPER
+const slider = document.querySelector(".swiper__cards");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", e => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mousemove", e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
