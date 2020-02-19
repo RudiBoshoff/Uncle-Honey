990bf755f7d818ec59679bdfd959129a4a168704
Rudi Boshoff <rudiboshoff15@gmail.com>
2020-02-19 13:47:11 +0200

    reworked swiper using swiper.js library

--- script.js
+++ script.js
@@ -14,31 +14,31 @@ $(function() {
   });
 });
 
-// SWIPER
-const slider = document.querySelector(".swiper__cards");
-let isDown = false;
-let startX;
-let scrollLeft;
+// VANILLA JS SWIPER
+// const slider = document.querySelector(".swiper__cards");
+// let isDown = false;
+// let startX;
+// let scrollLeft;
 
-slider.addEventListener("mousedown", e => {
-  isDown = true;
-  slider.classList.add("active");
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-slider.addEventListener("mouseup", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-slider.addEventListener("mousemove", e => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 2; //scroll-fast
-  slider.scrollLeft = scrollLeft - walk;
-  console.log(walk);
-});
+// slider.addEventListener("mousedown", e => {
+//   isDown = true;
+//   slider.classList.add("active");
+//   startX = e.pageX - slider.offsetLeft;
+//   scrollLeft = slider.scrollLeft;
+// });
+// slider.addEventListener("mouseleave", () => {
+//   isDown = false;
+//   slider.classList.remove("active");
+// });
+// slider.addEventListener("mouseup", () => {
+//   isDown = false;
+//   slider.classList.remove("active");
+// });
+// slider.addEventListener("mousemove", e => {
+//   if (!isDown) return;
+//   e.preventDefault();
+//   const x = e.pageX - slider.offsetLeft;
+//   const walk = (x - startX) * 2; //scroll-fast
+//   slider.scrollLeft = scrollLeft - walk;
+//   console.log(walk);
+// });
